78ad33f14ee474add3447156957f4d62d01b35de
krishna1234 <krish_cool6484@yahoo.com>
2024-11-14 18:37:15 +0530

    function to add numbers

---
+++ string-calculator/src/StringCalculator.jsx
@@ -0,0 +1,32 @@
+export function add(numbers) {
+    // handling if it is an empty string
+    if (numbers === "") {
+        console.log("Empty string");
+        return 0;
+    }
+
+    // here is the delimeter pattern which is regex pattern to match either a comma or new line charcter
+    const delimeter = /,|\n|;/;
+    console.log(delimeter);
+
+    // Split the numbers based on delimiters and convert to an array of numbers
+    let numberList = numbers.split(delimeter).map(Number);
+    console.log(numberList);
+
+    // check for negative numbers
+    const negativeNumber = numberList.filter((num) => num < 0);
+    console.log(negativeNumber);
+
+    if (negativeNumber.length > 0) {
+        console.log("Error as negative numbers");
+        throw new Error(
+            `negative numbers not allowed: ${negativeNumber.join(", ")}`
+        );
+    }
+
+    // Calculate the sum
+    const sum = numberList.reduce((sum, num) => sum + num, 0);
+    console.log(sum);
+
+    return sum;
+}
